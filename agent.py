dff3251599281f3cbb25786efde9c2fe792e5c3f
yangjian319 <yangjian319@gmial.com>
2018-07-12 17:36:12 +0800

    判断agent在哪个机房改为用socket去判断

--- agent.py
+++ agent.py
@@ -26,9 +26,18 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 
-# 判断本机属于哪个机房，并把机房ip写入本地文件中供插件上报使用
-# 这个过程需要12s左右
+# 日志
+LOG_FILE = "/data/Agent/log/agent.log"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fh = TimedRotatingFileHandler(LOG_FILE,when='D',interval=1,backupCount=30)
+datefmt = '%Y-%m-%d %H:%M:%S'
+format_str = '%(asctime)s %(levelname)s %(message)s '
+formatter = logging.Formatter(format_str, datefmt)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
+# 判断本机属于哪个机房，并把机房ip写入本地文件中供插件上报使用，这个过程需要12s左右
 iplist = ["172.30.130.137:18382", "172.30.130.126:18382", "10.124.5.163:18382", "10.144.2.248:18382", "10.123.30.177:18382", "172.30.194.121:18382", "172.16.5.20:18382", "10.181.1.0:18382"]
 for ip in iplist:
   try:
@@ -43,11 +52,10 @@ for ip in iplist:
     pass
 
 # 机房ip
-jifangip = currentip
-hearturl = "http://47.106.106.220:5000/register"
+jifangip = currentip  # 这个变量名想想再改
 plugin_dir = "/data/Agent/plugin/"
 
-# 创建日志文件目录
+# 创建日志文件目录，这个其实都不用，它在安装agent的时候不是要下发一个tar.gz包吗，目录直接在里面建好？
 logdir = "/data/Agent/log"
 if not os.path.exists(logdir):
     os.makedirs(logdir)
@@ -56,20 +64,6 @@ if not os.path.exists(logdir):
 address = ("0.0.0.0", 9998)
 udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udpsocket.bind(address)
-
-
-# 日志
-LOG_FILE = "/data/Agent/log/agent.log"
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-fh = TimedRotatingFileHandler(LOG_FILE,when='D',interval=1,backupCount=30)
-datefmt = '%Y-%m-%d %H:%M:%S'
-format_str = '%(asctime)s %(levelname)s %(message)s '
-formatter = logging.Formatter(format_str, datefmt)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-
 
 # 创建守护进程
 try:
@@ -92,7 +86,7 @@ except OSError, error:
   logging.info(msg)
   sys.exit(1)
 
-# 安装好agent的时候就上传一次md5值入库
+# 安装agent的时候就上传一次md5值入库
 def post_md5():
   (status, md5) = commands.getstatusoutput("sudo md5sum /root/.ssh/authorized_keys|awk '{print $1}'")
   req_data = {}
@@ -106,18 +100,18 @@ post_md5()
 
 
 def file_name(plugin_dir):
-  L = []
+  lsit = []
   for root, dirs, files in os.walk(plugin_dir):
     for file in files:
       if os.path.splitext(file)[1] == '.py':
-        L.append(file)
-  return L
+        list.append(file)
+  return list
 
 def sendFileName():
   while True:
     requrl = "http://" + jifangip + "/autoProxyPlugIn/sendFileName"
     names = file_name(plugin_dir)
-    name = {"names": names}
+    name = {"names": names} # 这里就是dic = {"names":["a","b"]} 格式
     name = urllib.urlencode(name)
     req = urllib2.Request(url=requrl, data=name)
     res = urllib2.urlopen(req)
@@ -137,7 +131,7 @@ def func():
       jifangip = fd.read()
 
     ips = allip.get_all_ips()
-    # {'ip': ['10.124.2.46', '10.148.138.133']}
+    # 格式{'ip': ['10.124.2.46', '10.148.138.133']}
     data = json.dumps(ips)
     headers = {"Content-Type": "application/json"}
     url_base1 = "http://" + jifangip + "/autoProxyPlugIn/sendIp"
@@ -156,9 +150,6 @@ except Exception, e:
 
 
 # 接收controller消息
-'''
-{u'status': 5, u'hostId': 3902, u'name': u'net', u'url': u'http://10.124.5.163:18382/proxyDownLoad/net_v03.py', u'plugId': 1, u'version': u'03', u'cycle': u''}
-'''
 while True:
   data, addr = udpsocket.recvfrom(2018)
   dic = json.loads(data)
